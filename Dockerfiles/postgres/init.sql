f5dc65fb5b6b427b348d7113196708c81a95e42f
julianov <julianov403@gmail.com>
2022-11-13 22:19:14 -0300

    pgAdmin/db-table-name

--- Dockerfiles/postgres/init.sql
+++ Dockerfiles/postgres/init.sql
@@ -1,24 +1,20 @@
 -- Init database for postgres, create user and database
 
 -- Create user
-CREATE USER docker WITH PASSWORD 'docker';
+CREATE USER postgres WITH PASSWORD 'postgres';
 
 -- Create database with id and name
-CREATE DATABASE docker WITH OWNER docker;
+CREATE DATABASE portalCiudadano WITH OWNER postgres;
 
-GRANT ALL PRIVILEGES ON DATABASE docker TO docker;
+GRANT ALL PRIVILEGES ON DATABASE portalCiudadano TO postgres;
 
 -- Create table to docker database with id and name
 \c docker
-CREATE TABLE docker (id SERIAL PRIMARY KEY, name VARCHAR(255));
+CREATE TABLE test (id SERIAL PRIMARY KEY, name VARCHAR(255));
 
 -- Insert data to docker table
-INSERT INTO docker (name) VALUES ('docker 1');
-INSERT INTO docker (name) VALUES ('docker 2');
-
--- Grant all privileges on database to user
-
-
+INSERT INTO test (name) VALUES ('docker 1');
+INSERT INTO test (name) VALUES ('docker 2');
 
 
 
