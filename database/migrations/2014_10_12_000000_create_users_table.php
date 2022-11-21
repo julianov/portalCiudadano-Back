345e7a02701037c14aa4e440ec636c0e0070901f
julianov <julianov403@gmail.com>
2022-11-21 17:32:39 -0300

    oracle-db

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
               
             $table->id();
-            $table->bigInteger('cuil')->unique();
+            $table->bigInteger('cuil')->unique()->primary();
             $table->string('nombre');
             $table->string('apellido');
             $table->string('email');
