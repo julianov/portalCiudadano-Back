baed3b39696c513861abd227012aa36f8d1507b1
joaquin <joaquin.messina@gmail.com>
2023-01-11 11:55:16 -0300

    Fix migration

--- database/migrations/2022_12_25_182508_user.php
+++ database/migrations/2022_12_25_182508_user.php
@@ -13,25 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-<<<<<<< HEAD:database/migrations/2014_10_12_000000_create_users_table.php
-        Schema::create('users', function (Blueprint $table) {
-              
-            $table->id();
-            $table->bigInteger('cuil')->unique()->primary();
-            $table->string('nombre');
-            $table->string('apellido');
-            $table->string('email');
-            $table->string('password');
-=======
         $a = Schema::create('users', function (Blueprint $table) {
             $table->id()->primary();
             $table->bigInteger('cuil')->unique();
             $table->bigInteger('prs_id')->unique();
             $table->string("email");
-            $table->string('password'); 
+            $table->string('password');
             $table->string('name'); #nombre de usuario
             $table->string('last_name'); #apellido de usuario
->>>>>>> e40bfe757f261588605a6116f2891d17defade28:database/migrations/2022_12_25_182508_user.php
 
             $table->timestamps(); //fixed
             $table->timestamp('email_verified_at')->nullable();
