34a6182bd3ecb739b2917eb2ad8b14e73b1d8e7a
julianov <38470377+julianov@users.noreply.github.com>
2023-01-27 22:48:39 -0300

    LocationsController, Locations services and locations maps

--- app/Http/Controllers/LocationsController.php
+++ app/Http/Controllers/LocationsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Services\WebServices\WsEntreRios\EntreRiosWSService;
+use Illuminate\Support\Facades\Validator;
 
 class LocationsController extends Controller
 {
@@ -15,9 +16,62 @@ class LocationsController extends Controller
 		$this->wsService = $wsService;
 	}
 
-    public function getLocations()
+    public function getLocations():JsonResponse
     {
+            
         $rs = $this->wsService->getERLocations();
-        return response()->json($rs);
+
+        if (sizeof($rs) > 1000) {
+
+            return response()->json($rs);
+            
+        }else{
+
+            return response()->json([
+                'status' => false,
+                'message' => 'WS temporarily unavailable'
+            ], 503);
+        }
+        
+    }
+
+    public function getStringLocations(Request $request):JsonResponse 
+    {
+
+        $validator = Validator::make($request->all(), [
+            'locality_id' => 'required',
+
+        ]);
+
+        if($validator->fails()){
+
+            return response()->json([
+                'status' => false,
+                'message' => 'Bad format'
+            ], 503);
+
+        }else{
+
+            $rs = $this->wsService->getERLocations();
+        
+            foreach ($rs as $item) {
+                
+                if ($item['ID']== $validator->getData()['locality_id']){
+
+                    return response()->json([
+                        'status' => true,
+                        'locality' => $item['NOMBRE'],
+                        'department' => $item['DEPARTAMENTO'],
+                    ], 200);
+
+                }
+            }
+            return response()->json([
+                'status' => false,
+                'message' => 'WS temporarily unavailable'
+            ], 503);
+
+        }
+        
     }
 }
