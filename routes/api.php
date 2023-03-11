7745f292f2e5273a400b5ac2f2b8b1e4777aa2d8
julianov <38470377+julianov@users.noreply.github.com>
2023-03-11 18:34:14 -0300

    Autenticar Afip endpoints

--- routes/api.php
+++ routes/api.php
@@ -41,7 +41,8 @@ Route::prefix("/v0/user")->controller(Controllers\UserController::class)->group(
 Route::middleware(['auth:authentication'])->get('/v0/er/locations', [Controllers\LocationsController::class, 'getLocations']);
 Route::middleware(['auth:authentication'])->get('/v0/er/getstringlocations', [Controllers\LocationsController::class, 'getStringLocations']);
 
-Route::get('/v0/getTokenAfip/{cuil}/', [Controllers\AuthController::class, 'getToken']);
+Route::get('/v0/getAfipUrl',[Controllers\AuthController::class, 'getUrlAfip']);
+Route::get('/v0/getTokenAfip/', [Controllers\AuthController::class, 'getToken']);
 
 
 Route::middleware(['auth:authentication','scope:level_1'])->post('/v0/testroute', [Controllers\UserController::class, 'test']);
