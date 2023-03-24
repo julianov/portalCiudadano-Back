35840290dc1389c01f57b41104dbf353b3552b18
julianov <julianov403@gmail.com>
2023-03-24 16:43:36 -0300

    message output at check cuil

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -6,5 +6,36 @@ use Illuminate\Http\Request;
 
 class NotificationsController extends Controller
 {
-    //
+
+    public function newNotification(Request $request){
+
+        $request->validate([
+            "token" => "required|string",
+        ]);
+
+        $host = env('BASEUR_ER_WS_TOKEN');
+
+        $data = [
+            '_tk' => $request['token']
+           ];
+
+        $response = $client->request('POST', $host, [
+            'headers' => [
+            'Content-Type' => 'application/json'
+        ],
+            'json' => $data
+        ]);
+
+        if($response == 0){
+
+        }else{
+
+            return response()->json([
+                'status' => false,
+                'message' => 'Bad token'
+            ], 401);
+
+        }
+    }
+
 }
