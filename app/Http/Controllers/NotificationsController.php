5bdedbbf34f96bcb9f3ce1498bb16736a15b2e25
julianov <julianov403@gmail.com>
2023-04-23 15:17:24 -0300

    date format fixed

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -107,7 +107,7 @@ class NotificationsController extends Controller
 
                 $table_name = "NOTIFICATIONS";
                 $columns = "RECIPIENTS, AGE_FROM, AGE_TO, DEPARTMENT_ID, LOCALITY_ID, MESSAGE_TITLE, MESSAGE_BODY, ATTACHMENT_TYPE,ATTACHMENT, NOTIFICATION_DATE_FROM, NOTIFICATION_DATE_TO, SEND_BY_EMAIL,CREATED_AT";
-                $values = "'".$validated['recipients']."',".$validated['age_from'].",".$validated['age_to'].",".$validated['department_id'].",".$validated['locality_id'].",'".$validated['message_title']."','".$validated['message_body']."','".$validated['attachment_type']."','".$validated['attachment']."','".$validated['notification_date_from']."','".$validated['notification_date_to']."','".$validated['send_by_email']."',sysdate";
+                $values = "'".$validated['recipients']."',".$validated['age_from'].",".$validated['age_to'].",".$validated['department_id'].",".$validated['locality_id'].",'".$validated['message_title']."','".$validated['message_body']."','".$validated['attachment_type']."','".$validated['attachment']."',"."(TO_DATE('".$validated['notification_date_from']."', 'DD/MM/YYYY')),"."(TO_DATE('".$validated['notification_date_to']."', 'DD/MM/YYYY'))".",'".$validated['send_by_email']."',sysdate";
 
                 $res= $this->plSqlServices->insertarFila($table_name, $columns, $values);
 
@@ -137,7 +137,8 @@ class NotificationsController extends Controller
                 
                 $table_name = "NOTIFICATIONS";
                 $columns = "RECIPIENTS, AGE_FROM, AGE_TO, DEPARTMENT_ID, LOCALITY_ID, MESSAGE_TITLE, MESSAGE_BODY, ATTACHMENT_TYPE, NOTIFICATION_DATE_FROM, NOTIFICATION_DATE_TO, SEND_BY_EMAIL,CREATED_AT";
-                $values = "'".$validated['recipients']."',".$validated['age_from'].",".$validated['age_to'].",".$validated['department_id'].",".$validated['locality_id'].",'".$validated['message_title']."','".$validated['message_body']."','".$validated['attachment_type']."','".$validated['notification_date_from']."','".$validated['notification_date_to']."','".$validated['send_by_email']."',sysdate";
+                $values = "'".$validated['recipients']."',".$validated['age_from'].",".$validated['age_to'].",".$validated['department_id'].",".$validated['locality_id'].",'".$validated['message_title']."','".$validated['message_body']."','".$validated['attachment_type']."',"."(TO_DATE('".$validated['notification_date_from']."', 'DD/MM/YYYY')),"."(TO_DATE('".$validated['notification_date_to']."', 'DD/MM/YYYY'))".",'".$validated['send_by_email']."',sysdate";
+
                 $res= $this->plSqlServices->insertarFila($table_name, $columns, $values);
 
                 if ($res){
@@ -146,7 +147,7 @@ class NotificationsController extends Controller
 
                         self::sendNotificationsEmails($validated['recipients'],$validated['age_from'],$validated['age_to'],$validated['department_id'],$validated['locality_id'],$validated['message_title'],$validated['message_body'],$validated['attachment_type'],"none",$validated['notification_date_from'],$validated['notification_date_to']);
                     
-                    }
+                   }
 
                     return response()->json([
                         'status' => true,
@@ -180,7 +181,7 @@ class NotificationsController extends Controller
             if (!empty($user_data)) {
 
                 //$fecha_val, $departamento_val, $localidad_val, $edad_val, $destinatario_val
-                $fechaActual = Carbon::now()->format('d/m/y');
+                $fechaActual = Carbon::now()->format('d/m/Y');
                 $fechaCumpleanos = Carbon::parse($user_data->BIRTHDAY);
                 // Calcular la edad
                 $edad = $fechaCumpleanos->age;
