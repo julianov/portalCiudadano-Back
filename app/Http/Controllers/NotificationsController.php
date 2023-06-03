aa0bc8dd32aa682068846b63569bebb732cbe047
julianov <julianov403@gmail.com>
2023-06-03 13:16:21 -0300

    fix many attachments

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -44,7 +44,7 @@ class NotificationsController extends Controller
         $usuarios_unicos = array_unique($usuarios);
         if (count($usuarios_unicos) > 0 ){
 
-            if (count($attachments)!=0){
+            if (count($attachments) !== 0 && $attachments !== 'none'){
 
                 foreach ($usuarios_unicos as $usuario) {
                     $mail = (new NotificationEmail($message_title, $message_body))
@@ -171,15 +171,16 @@ class NotificationsController extends Controller
                
             }else{
                 
+
                 $table_name = "NOTIFICATIONS";
-                $columns = "RECIPIENTS, AGE_FROM, AGE_TO, DEPARTMENT_ID, LOCALITY_ID, MESSAGE_TITLE, MESSAGE_BODY, ATTACHMENT_TYPE, NOTIFICATION_DATE_FROM, NOTIFICATION_DATE_TO, SEND_BY_EMAIL,CREATED_AT";
+                $columns = "RECIPIENTS, AGE_FROM, AGE_TO, DEPARTMENT_ID, LOCALITY_ID, MESSAGE_TITLE, MESSAGE_BODY, NOTIFICATION_DATE_FROM, NOTIFICATION_DATE_TO, SEND_BY_EMAIL,CREATED_AT";
                 
                 $send_email_validation='0';
 
                 if ($validated['send_by_email']=="true"){
                     $send_email_validation='1';
                 }
-                $values = "'".$validated['recipients']."',".$validated['age_from'].",".$validated['age_to'].",".$validated['department_id'].",".$validated['locality_id'].",'".$validated['message_title']."','".$validated['message_body']."','"."none"."',"."(TO_DATE('".$validated['notification_date_from']."', 'DD/MM/YYYY')),"."(TO_DATE('".$validated['notification_date_to']."', 'DD/MM/YYYY'))".",'".$send_email_validation."',sysdate";
+                $values = "'".$validated['recipients']."',".$validated['age_from'].",".$validated['age_to'].",".$validated['department_id'].",".$validated['locality_id'].",'".$validated['message_title']."','".$validated['message_body']."',"."(TO_DATE('".$validated['notification_date_from']."', 'DD/MM/YYYY')),"."(TO_DATE('".$validated['notification_date_to']."', 'DD/MM/YYYY'))".",'".$send_email_validation."',sysdate";
 
                 $res= $this->plSqlServices->insertarFila($table_name, $columns, $values);
 
@@ -187,9 +188,9 @@ class NotificationsController extends Controller
 
                     if ($validated['send_by_email']=="true"){
 
-                        self::sendNotificationsEmails($validated['recipients'],$validated['age_from'],$validated['age_to'],$validated['department_id'],$validated['locality_id'],$validated['message_title'],$validated['message_body'],"none","none",$validated['notification_date_from'],$validated['notification_date_to']);
-                    
-                   }
+                        self::sendNotificationsEmails($validated['recipients'],$validated['age_from'],$validated['age_to'],$validated['department_id'],$validated['locality_id'],$validated['message_title'],$validated['message_body'],"none",$validated['notification_date_from'],$validated['notification_date_to']);
+                   
+                    }
 
                     return response()->json([
                         'status' => true,
