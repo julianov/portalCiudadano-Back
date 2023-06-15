22189cc9c137e0f5458c6363b83f82f46c8b9cea
julianov <julianov403@gmail.com>
2023-06-15 14:25:11 -0300

    new password request

--- app/Http/Requests/User/PasswordResetRequest.php
+++ app/Http/Requests/User/PasswordResetRequest.php
@@ -25,7 +25,7 @@ class PasswordResetRequest extends FormRequest
 	{
 		return [
 			'token' => 'required',
-			'password' => 'required|string|min:8'
+			'new_password' => 'required|string|min:8'
 	
 		];
 	}
