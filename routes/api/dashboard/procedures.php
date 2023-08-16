78b522f7a1df498256729a990234a087aaaa70f8
Grecco, Franco <srgrecco93@gmail.com>
2023-08-15 23:54:13 -0300

    fix(prodecure-data): enable auth middleware, move forms to own controller

--- routes/api/dashboard/procedures.php
+++ routes/api/dashboard/procedures.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProcedureDataController as Controller;
 
 Route::prefix('/procedures')
-    // ->middleware(['auth:authentication'])
+    ->middleware(['auth:authentication'])
     ->controller(Controller::class)
     ->group(function () {
         Route::get('/', 'getList');
@@ -14,8 +14,4 @@ Route::prefix('/procedures')
         Route::get('/getById', 'getById');
         Route::post('/update', 'updateById');
         Route::post('/delete', 'deleteById');
-
-        Route::controller(FormDataController::class)->prefix('/forms');
-        Route::post('/forms', 'createForm');
-        Route::post('/forms/update', 'updateForm');
     });
